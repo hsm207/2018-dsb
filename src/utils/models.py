98fe47aafd0511bc8d85224b3c0c3479917bc5e8
hsm207 <hsm207@users.noreply.github.com>
2018-02-17 05:36:48 -0500

    Created classes to implement the Rib Cage architecture.

--- src/utils/models.py
+++ src/utils/models.py
@@ -2,8 +2,9 @@
 This module contains classes that implement various model architectures for both the generator and discriminator
 """
 import tensorflow as tf
+from tensorflow.python.keras.layers import Dense, LeakyReLU
 
-from utils.layers import ConvBlock
+from utils.layers import ConvBlock, RibCage, ConvToFcAdapter
 
 
 class ConvAssaf:
@@ -57,3 +58,60 @@ class ConvAssaf:
         x = self.conv5(x)
 
         return x
+
+
+class FullyConvRibCage:
+    def __init__(self, data_format='channels_first', alpha=0.1):
+        """
+        A variant of the Ribcage Discriminator described in the "MICROSCOPY CELL SEGMENTATION VIA ADVERSARIAL
+        NEURAL NETWORKS" paper by Assaf Arbelle and Tammy Riklin Raviv
+
+        This version uses global average pooling on the output of the last rib cage to make the model work
+        on arbitrary sized images
+
+        :param data_format: The data format of the input to the convolution and max pool layers
+        :param alpha: The parameter to the leaky ReLu activation function
+        """
+        self.data_format = data_format
+        self.channel_axis = 1 if data_format == 'channels_first' else -1
+        self.alpha = alpha
+
+    def _build_layers(self):
+        self.rib1 = tf.make_template('rib1', RibCage(kernel_size=3, filters=32, data_format=self.data_format))
+        self.rib2 = tf.make_template('rib2', RibCage(kernel_size=3, filters=64, data_format=self.data_format))
+        self.rib3 = tf.make_template('rib3', RibCage(kernel_size=3, filters=128, data_format=self.data_format))
+
+        self.relu = LeakyReLU(alpha=self.alpha)
+        self.conv_to_fc = tf.make_template('flatten_conv',
+                                           ConvToFcAdapter(output=97344, data_format=self.data_format))
+
+        self.dense1 = Dense(64, use_bias=False, activation='linear', name='dense1')
+        self.dense2 = Dense(64, use_bias=False, activation='linear', name='dense2')
+        self.dense3 = Dense(1, use_bias=False, activation='sigmoid', name='dense3')
+
+    def __call__(self, inputs, conditioning_inputs):
+        self._build_layers()
+
+        masks = inputs
+        images = conditioning_inputs
+        concat_images_masks = tf.concat([images, masks], axis=self.channel_axis)
+
+        # pass the inputs through the rib blocks
+        left1, right1, center1 = self.rib1(images, masks, concat_images_masks)
+        left2, right2, center2 = self.rib2(left1, right1, center1)
+        left3, right3, center3 = self.rib3(left2, right2, center2)
+
+        # flatten the output from the rib blocks using global average pooling
+        ribs_output = tf.concat([left3, right3, center3], axis=self.channel_axis)
+        ribs_output = self.conv_to_fc(ribs_output)
+
+        # pass the results through to the dense layers
+        ribs_output = self.dense1(ribs_output)
+        ribs_output = self.relu(ribs_output)
+
+        ribs_output = self.dense2(ribs_output)
+        ribs_output = self.relu(ribs_output)
+
+        ribs_output = self.dense3(ribs_output)
+
+        return ribs_output
